bd0cfd2bb4264ebe5fdd54e4ad2398aa41781b2e
TJ Horner <me@tjhorner.com>
2020-11-20 01:01:27 -0500

    hahaha

---
+++ WhereIsForward/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WhereIsForward
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new WhereIsForwardApp();
+
+            Console.WriteLine("Hello gamer. You now have an arrow. Congratulation! :) Press enter to exit.");
+            Console.ReadLine();
+
+            app.Shutdown();
+        }
+    }
+}
---
+++ WhereIsForward/WhereIsForwardApp.cs
@@ -0,0 +1,48 @@
+﻿using OVRUtils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WhereIsForward
+{
+    class WhereIsForwardApp
+    {
+        private readonly Application app;
+        private readonly Overlay floorOverlay;
+
+        public WhereIsForwardApp()
+        {
+            app = new Application(Application.ApplicationType.Overlay);
+            floorOverlay = new Overlay("where_is_forward", "Where Is Forward????")
+            {
+                WidthInMeters = 0.1f,
+
+                Transform = new Valve.VR.HmdMatrix34_t
+                {
+                    m0 = 1,
+                    m1 = 0,
+                    m2 = 0,
+                    m3 = 0,
+                    m4 = 0,
+                    m5 = 0,
+                    m6 = 1,
+                    m7 = 0,
+                    m8 = 0,
+                    m9 = -1,
+                    m10 = 0,
+                    m11 = 0
+                }
+            };
+
+            var overlayImagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Resources\arrow.png");
+            floorOverlay.SetTextureFromFile(overlayImagePath);
+            floorOverlay.Show();
+        }
+
+        public void Shutdown()
+        {
+            app.Shutdown();
+        }
+    }
+}
