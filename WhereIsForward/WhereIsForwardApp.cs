f5c87c1615545a2e21e204cde1393cd84edf60e8
TJ Horner <me@tjhorner.com>
2021-07-12 21:06:54 -0400

    refactor stuffs

--- WhereIsForward/WhereIsForwardApp.cs
+++ WhereIsForward/WhereIsForwardApp.cs
@@ -2,43 +2,15 @@
 
 namespace WhereIsForward
 {
-    class WhereIsForwardApp
+    class WhereIsForwardApp : Application
     {
-        private readonly Application app;
         private readonly Overlay floorOverlay;
 
         public WhereIsForwardApp()
+            : base(ApplicationType.Overlay)
         {
-            app = new Application(Application.ApplicationType.Overlay);
-
-            floorOverlay = new Overlay("where_is_forward", "Where Is Forward????")
-            {
-                WidthInMeters = 0.1f,
-                Transform = new Valve.VR.HmdMatrix34_t
-                {
-                    m0 = 1,
-                    m1 = 0,
-                    m2 = 0,
-                    m3 = 0,
-                    m4 = 0,
-                    m5 = 0,
-                    m6 = 1,
-                    m7 = 0,
-                    m8 = 0,
-                    m9 = -1,
-                    m10 = 0,
-                    m11 = 0
-                }
-            };
-
-            var overlayImagePath = Utils.PathToResource("arrow.png");
-            floorOverlay.SetTextureFromFile(overlayImagePath);
+            floorOverlay = new WhereIsForwardOverlay();
             floorOverlay.Show();
-        }
-
-        public void Shutdown()
-        {
-            app.Shutdown();
         }
     }
 }
---
+++ WhereIsForward/WhereIsForwardOverlay.cs
@@ -0,0 +1,28 @@
+﻿using OVRSharp;
+using OVRSharp.Math;
+using System;
+using System.Numerics;
+
+namespace WhereIsForward
+{
+    class WhereIsForwardOverlay : Overlay
+    {
+        public WhereIsForwardOverlay()
+            : base("where_is_forward", "Where Is Forward????")
+        {
+            WidthInMeters = 0.1f;
+
+            Transform = Matrix4x4
+                .CreateRotationX(DegreesToRadians(90f))
+                .ToHmdMatrix34_t();
+
+            var overlayImagePath = Utils.PathToResource("arrow.png");
+            SetTextureFromFile(overlayImagePath);
+        }
+
+        private static float DegreesToRadians(float degrees)
+        {
+            return (float)(degrees * (Math.PI / 180f));
+        }
+    }
+}
